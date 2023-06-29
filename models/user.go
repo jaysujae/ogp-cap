392b3d955d297486f79a681e6cb3b11a0c3f2f7a
jaysujae <jaysujae@gmail.com>
2023-06-29 11:48:17 +0800

    add embedding distance and sort by it

--- models/user.go
+++ models/user.go
@@ -1,19 +1,39 @@
 package models
 
 import (
+	"context"
+	"database/sql/driver"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/PullRequestInc/go-gpt3"
 	"github.com/jinzhu/gorm"
+	"math"
 	"math/rand"
+	"sort"
 )
+
+type CustomFloats struct {
+	Floats []float64
+}
+
+func (c *CustomFloats) Scan(value interface{}) error {
+	return json.Unmarshal(value.([]byte), &c.Floats)
+}
+
+func (c CustomFloats) Value() (driver.Value, error) {
+	return json.Marshal(c.Floats)
+}
 
 // User defines the shape of the user table in the database
 type User struct {
 	gorm.Model
-	Nickname     string
-	Introduction string `gorm:"not null"`
-	Image        string
-	Chats        []Chat
-	Comments     []Comment
+	Nickname          string
+	Introduction      string `gorm:"not null"`
+	Image             string
+	CustomFloatsValue CustomFloats `gorm:"type:json"`
+	Chats             []Chat
+	Comments          []Comment
 }
 
 // UserService defines all methods of the user service
@@ -34,11 +54,66 @@ func NewUserGorm(db *gorm.DB) *UserGorm {
 	}
 }
 func (ug *UserGorm) GetGroupUsersByID(id uint) (*[]User, error) {
-	users := &[]User{}
-	if err := ug.db.Where("id <= ? AND id >= ?", id+5, id-5).Find(users).Error; err != nil {
+	var currentUser User
+	if err := ug.db.Where("id = ?", id).First(&currentUser).Error; err != nil {
 		return nil, err
 	}
-	return users, nil
+
+	users := []User{}
+	if err := ug.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+
+	type UserWithValue struct {
+		User
+		Value float64
+	}
+
+	usersWithValue := make([]UserWithValue, len(users))
+	for i, user := range users {
+		val, _ := Cosine(user.CustomFloatsValue.Floats, currentUser.CustomFloatsValue.Floats)
+		usersWithValue[i] = UserWithValue{User: user, Value: val}
+	}
+
+	sort.Slice(usersWithValue, func(i, j int) bool {
+		return usersWithValue[i].Value > usersWithValue[j].Value
+	})
+	for i := range users {
+		users[i] = usersWithValue[i].User
+	}
+
+	return &users, nil
+}
+
+func Cosine(a []float64, b []float64) (cosine float64, err error) {
+	count := 0
+	length_a := len(a)
+	length_b := len(b)
+	if length_a > length_b {
+		count = length_a
+	} else {
+		count = length_b
+	}
+	sumA := 0.0
+	s1 := 0.0
+	s2 := 0.0
+	for k := 0; k < count; k++ {
+		if k >= length_a {
+			s2 += math.Pow(b[k], 2)
+			continue
+		}
+		if k >= length_b {
+			s1 += math.Pow(a[k], 2)
+			continue
+		}
+		sumA += a[k] * b[k]
+		s1 += math.Pow(a[k], 2)
+		s2 += math.Pow(b[k], 2)
+	}
+	if s1 == 0 || s2 == 0 {
+		return 0.0, errors.New("Vectors should not be null (all zeros)")
+	}
+	return sumA / (math.Sqrt(s1) * math.Sqrt(s2)), nil
 }
 
 func (ug *UserGorm) Create(user *User) error {
@@ -73,6 +148,14 @@ func (ug *UserGorm) Create(user *User) error {
 	nameRand := rand.Intn(10)
 	user.Nickname = adjectives[adjRand] + " " + animals[nameRand]
 	user.Image = animals[nameRand] + ".jpg"
+
+	embedding, _ := Client.Embeddings(context.TODO(), gpt3.EmbeddingsRequest{
+		Input: []string{user.Introduction},
+		Model: "text-embedding-ada-002",
+		User:  fmt.Sprint(user.ID),
+	})
+	user.CustomFloatsValue = CustomFloats{Floats: embedding.Data[0].Embedding}
+
 	return ug.db.Create(&user).Error
 }
 
